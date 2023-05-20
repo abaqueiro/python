04df37003e44d1667933d8f13e9437eeed53f472
Alfonso Baqueiro Bernal <abaqueiro@gmail.com>
2023-05-19 21:19:08 -0600

    Example 1 	new file:   fcc/e01-reserved-words.py

---
+++ fcc/e01-reserved-words.py
@@ -0,0 +1,12 @@
+# comment till the end of line
+print( '=====================================================' )
+print( '    Reserved Words:' )
+print( '=====================================================' )
+print( '  False    class    return    is        finally' )
+print( '  None     if       for       lambda    continue' )
+print( '  True     def      from      while     nonlocal' )
+print( '  and      del      global    not       with' )
+print( '  as       elif     try       or        yield' )
+print( '  assert   else     import    pass' )
+print( '  break    except   in        raise' )
+
