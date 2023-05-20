b5ecae2c8cc8452d02759a0eb689533f9ece45d7
Alfonso Baqueiro Bernal <abaqueiro@gmail.com>
2023-05-19 22:26:37 -0600

    Example on types and operators, evaluation priority, and type conversion.
    	new file:   fcc/e03-types-and-operators.py

---
+++ fcc/e03-types-and-operators.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# the type function
+x = 3
+print('x = 3')
+print('type(x) returns', type(x))
+print()
+
+x = 3.1415
+print('x = 3.1415')
+print('type(x) returns', type(x))
+print()
+
+x = 'word'
+print("x = 'word'")
+print('type(x) returns', type(x))
+print()
+
+# booleans
+x = True
+print('x = True')
+print('type(x) returns', type(x))
+print()
+
+x = False
+print('x = False')
+print('type(x) returns', type(x))
+print()
+
+# None type, similar to the well known null
+x = None
+print('x = None')
+print('type(x) returns', type(x))
+print()
+
+print('# since python 3, division of integers returns float')
+print('10/5 returns', 10/5)
+print('7/2 returns', 7/2)
+print()
+
+# some arithmetic operators
+print('# Arithmetic operators: ')
+print('5 + 2 returns', 5 + 2)
+print('5 - 2 returns', 5 - 2)
+print('5 * 5 returns', 5 * 5)
+print('25 / 5 returns', 25 / 5)
+# power
+print('2**8 returns',2 ** 8) # prints 256
+# residue
+print('5%2 returns', 5 % 2) # prints 1
+print()
+
+print('# Order of Evaluation')
+print('1 parenthesis')
+print('2 remainder')
+print('3 power')
+print('4 multiplication or division')
+print('5 sum or substraction')
+print('6 evaluated Left to Right')
+print()
+
+# type convertion
+print('# Type conversion')
+print( 'float(3) returns', float(3) )
+print( "int('25') returns", int('25') )
+
